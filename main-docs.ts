fc61c9c39cc0ebac1a38557f28145396a1daa2f5
Petyo Ivanov <underlog@gmail.com>
2017-08-15 17:08:16 +0300

    remove bogus import, unnecessary

--- main-docs.ts
+++ main-docs.ts
@@ -1,7 +1,6 @@
 import "./ag-grid/src/styles/ag-grid.scss";
 import "./ag-grid/src/styles/theme-fresh.scss";
+import "./ag-grid/src/styles/theme-material-next.scss";
 
 import "./ag-grid-enterprise/src/main.ts";
 export * from "./ag-grid/exports";
-
-import "./ag-grid-docs/src/example.js";
